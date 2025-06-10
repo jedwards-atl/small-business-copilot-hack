efae880ea83cd1d93e09bc6aa04b373eab3a5f3d
Jake Edwards <jake.edwards@simplybusiness.com>
2025-06-10 09:58:27 -0400

    create Navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const navItems = [
+  { label: "Features", href: "/features" },
+  { label: "Demo", href: "/demo" },
+  { label: "How To Get It", href: "/how-to-get-it" },
+  { label: "Pricing", href: "/pricing" },
+  { label: "Partners", href: "/partners" },
+];
+
+const Navbar = () => {
+  const pathname = usePathname();
+
+  return (
+    <nav className="flex items-center justify-between mx-auto w-full px-14 py-4 max-sm:px-4">
+      <div className="flex items-center cursor-pointer">
+        <Link href="/">
+          <Image src="/globe.svg" alt="Logo" width={40} height={40} />
+        </Link>
+      </div>
+
+      <div className="flex items-center gap-2">
+        {navItems.map(({ label, href }) => (
+          <Link
+            key={label}
+            href={href}
+            className={cn(pathname === href && "font-semibold")}
+          >
+            {label}
+          </Link>
+        ))}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
