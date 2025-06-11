134e78f5c6348af1fd36cb1a3a4cea7cfb46a713
melissa.teng <melissa.teng@simplybusiness.co.uk>
2025-06-11 11:07:30 +0100

    feat: create mcp

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -2,7 +2,8 @@ import { createAmazonBedrock } from "@ai-sdk/amazon-bedrock"
 import { fromSSO } from "@aws-sdk/credential-providers";
 import { streamText } from "ai";
 import { NextResponse } from "next/server";
-import { useChat } from '@ai-sdk/react'
+import { experimental_createMCPClient, generateText } from 'ai';
+import { Experimental_StdioMCPTransport } from 'ai/mcp-stdio';
 
 const bedrock = createAmazonBedrock({
     region: 'us-east-1',
@@ -21,6 +22,20 @@ export async function POST(request: Request) {
     try {
         const { messages } = await request.json();
 
+        const transport = new Experimental_StdioMCPTransport({
+            command: "bunx",
+            args: ["@dhravya/apple-mcp@latest"]
+        });
+        const clientOne = await experimental_createMCPClient({
+            transport,
+        });
+
+        const toolSetOne = await clientOne.tools();
+
+        const tools = {
+            ...toolSetOne
+        };
+
         if (!messages) {
             return NextResponse.json(
                 { error: "Prompt is required" },
@@ -31,6 +46,7 @@ export async function POST(request: Request) {
         const response = streamText({
             model: bedrock('anthropic.claude-3-5-sonnet-20240620-v1:0'),
             system: 'You are a helpful assistant. Use tools as needed. Format your responses elegantly, concisely, and in a way that is easy to read..',
+            tools: tools,
             messages: messages,
             maxSteps: 5,
         });
