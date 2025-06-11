4d84e8b722db73e9d280587ebc0dd54e1bdba4f9
Ahad Rahman <ahad.rahman@simplybusiness.co.uk>
2025-06-11 11:24:47 +0100

    attempt to add shopify mcp

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,62 +1,83 @@
-import { createAmazonBedrock } from "@ai-sdk/amazon-bedrock"
+import { createAmazonBedrock } from "@ai-sdk/amazon-bedrock";
 import { fromSSO } from "@aws-sdk/credential-providers";
 import { streamText } from "ai";
 import { NextResponse } from "next/server";
-import { experimental_createMCPClient, generateText } from 'ai';
-import { Experimental_StdioMCPTransport } from 'ai/mcp-stdio';
+import { experimental_createMCPClient, generateText } from "ai";
+import { Experimental_StdioMCPTransport } from "ai/mcp-stdio";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const bedrock = createAmazonBedrock({
-    region: 'us-east-1',
-    credentialProvider: fromSSO({
-        ssoStartUrl: 'https://d-93676304be.awsapps.com/start',
-        ssoAccountId: '339712779037',
-        ssoRoleName: "llm-dev-bedrock",
-        ssoRegion: "eu-west-1",
-        ssoSession: 'bnw-sso',
-        profile: 'dev-bedrock'
-    }),
+  region: "us-east-1",
+  credentialProvider: fromSSO({
+    ssoStartUrl: "https://d-93676304be.awsapps.com/start",
+    ssoAccountId: "339712779037",
+    ssoRoleName: "llm-dev-bedrock",
+    ssoRegion: "eu-west-1",
+    ssoSession: "bnw-sso",
+    profile: "dev-bedrock",
+  }),
 });
 
-
 export async function POST(request: Request) {
-    try {
-        const { messages } = await request.json();
+  try {
+    const { messages } = await request.json();
 
-        const transport = new Experimental_StdioMCPTransport({
-            command: "bunx",
-            args: ["@dhravya/apple-mcp@latest"]
-        });
-        const clientOne = await experimental_createMCPClient({
-            transport,
-        });
+    // Apple
+    const apple_transport = new Experimental_StdioMCPTransport({
+      command: "bunx",
+      args: ["@dhravya/apple-mcp@latest"],
+    });
+    const apple_clientOne = await experimental_createMCPClient({
+      transport: apple_transport,
+    });
 
-        const toolSetOne = await clientOne.tools();
+    // Apple
+    const shopify_transport = new Experimental_StdioMCPTransport({
+      command: "npx",
+      args: [
+        "shopify-mcp",
+        "--accessToken",
+        process.env.SHOPIFY_ACCESS_TOKEN as string,
+        "--domain",
+        "0pi94v-h9.myshopify.com",
+      ],
+    });
+    const shopify_clientOne = await experimental_createMCPClient({
+      transport: shopify_transport,
+    });
 
-        const tools = {
-            ...toolSetOne
-        };
+    const apple_toolSetOne = await apple_clientOne.tools();
+    const shopify_toolSetOne = await shopify_clientOne.tools();
 
-        if (!messages) {
-            return NextResponse.json(
-                { error: "Prompt is required" },
-                { status: 400 }
-            );
-        }
+    const tools = {
+      ...apple_toolSetOne,
+      ...shopify_toolSetOne,
+    };
 
-        const response = streamText({
-            model: bedrock('anthropic.claude-3-5-sonnet-20240620-v1:0'),
-            system: 'You are a helpful assistant. Use tools as needed. Format your responses elegantly, concisely, and in a way that is easy to read..',
-            tools: tools,
-            messages: messages,
-            maxSteps: 5,
-        });
-
-        return response.toDataStreamResponse()
-    } catch (error) {
-        console.error("Error:", error);
-        return NextResponse.json(
-            { error: "Failed to process request" },
-            { status: 500 }
-        );
+    if (!messages) {
+      return NextResponse.json(
+        { error: "Prompt is required" },
+        { status: 400 }
+      );
     }
+
+    const response = streamText({
+      model: bedrock("anthropic.claude-3-5-sonnet-20240620-v1:0"),
+      system:
+        "You are a helpful assistant. Use tools as needed. Format your responses elegantly, concisely, and in a way that is easy to read..",
+      tools: tools,
+      messages: messages,
+      maxSteps: 5,
+    });
+
+    return response.toDataStreamResponse();
+  } catch (error) {
+    console.error("Error:", error);
+    return NextResponse.json(
+      { error: "Failed to process request" },
+      { status: 500 }
+    );
+  }
 }
