b47e5593c55b08b134cc509030210aed8f56ea3c
Jake Edwards <jake.edwards@simplybusiness.com>
2025-06-11 15:05:57 -0400

    create BusinessGrowthPage

---
+++ app/(app)/business-growth/page.tsx
@@ -0,0 +1,140 @@
+import Upcoming from "@/components/task-manager-card-content/Upcoming";
+import { Button } from "@/components/ui/button";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import {
+  Bell,
+  Calendar1,
+  Heart,
+  Lightbulb,
+  LucideProps,
+  MoreHorizontal,
+} from "lucide-react";
+import { title } from "process";
+import React from "react";
+
+const tiles = [
+  {
+    title: "Business Insurance",
+    subtitle: "Congrats on the new warehouse!",
+    description:
+      "Hi Kiron, great news about the new warehouse. A new location can change your insurance needs, especially if you're hiring new staff. Let's make sure you're covered.",
+    icon: Bell,
+    appsUsed: [
+      { title: "Apple Calendar", icon: Calendar1 },
+      { title: "Apple Calendar", icon: Calendar1 },
+    ],
+    deepDiveText: "Review Coverage",
+  },
+  {
+    title: "Data Driven Insights",
+    subtitle: "Predictive Sales Forecasting",
+    description:
+      "Let's turn your sales data and market trends into accurate forecasts that make budgeting and planning simple.",
+    icon: Lightbulb,
+    appsUsed: [],
+    deepDiveText: "Get Started",
+  },
+  {
+    title: "Integration to Social Media",
+    subtitle: "Turn posts into new customers",
+    description:
+      "Find your most important posts and get clear steps to make them more effective.",
+    icon: Heart,
+    appsUsed: [],
+    deepDiveText: "Get Started",
+  },
+];
+
+const BusinessGrowthPage = () => {
+  const tile = ({
+    title,
+    subtitle,
+    description,
+    appsUsed,
+    icon: Icon,
+    deepDiveText,
+  }: {
+    title: string;
+    subtitle: string;
+    description: string;
+    appsUsed: {
+      title: string;
+      icon: React.ForwardRefExoticComponent<
+        Omit<LucideProps, "ref"> & React.RefAttributes<SVGSVGElement>
+      >;
+    }[];
+    icon: React.ForwardRefExoticComponent<
+      Omit<LucideProps, "ref"> & React.RefAttributes<SVGSVGElement>
+    >;
+    deepDiveText: string;
+  }) => {
+    return (
+      <Card className="shadow-lg border-none h-full">
+        <CardHeader className="flex flex-row items-center justify-between">
+          <CardTitle className="flex flex-row items-center gap-4">
+            <Icon className="h-4 w-4 text-purple-600" />
+            <p className="text-lg font-bold text-gray-900">{title}</p>
+          </CardTitle>
+          <Button variant="ghost" size="icon" className="cursor-pointer">
+            <MoreHorizontal className="h-4 w-4" />
+          </Button>
+        </CardHeader>
+        <CardContent>
+          <div className="flex flex-col gap-10">
+            <h3 className="text-md font-bold text-purple-800">{subtitle}</h3>
+
+            <p className="text-gray-900">{description}</p>
+
+            <div className="flex flex-row gap-6">
+              {appsUsed.map(({ title, icon: Icon }, index) => (
+                <div>
+                  <p className="text-sm text-gray-600 font-medium mb-3">
+                    App Used:
+                  </p>
+                  <div className="flex items-center space-x-3">
+                    <Icon className="w-8 h-8 text-purple-600" />
+                    {/* <div className="w-2 h-2 bg-purple-300 rounded-full group-hover:bg-gray-400"></div> */}
+                    <span className="text-md text-gray-900 truncate">
+                      {title}
+                    </span>
+                  </div>
+                </div>
+              ))}
+            </div>
+
+            <p className="text-purple-800 font-normal border-b-1 border-purple-800 w-fit cursor-pointer hover:font-medium">
+              {deepDiveText}
+            </p>
+          </div>
+        </CardContent>
+      </Card>
+    );
+  };
+
+  return (
+    <div>
+      {/* Content Title & Description */}
+      <div className="flex flex-col gap-4 mb-8">
+        <h1 className="text-2xl font-semibold text-gray-900">
+          Support for your growing business
+        </h1>
+        <p className="text-gray-900">
+          Business growth means more to manage. SimplyPilot helps you handle the
+          complexity, so you can focus on what's next.
+        </p>
+      </div>
+
+      {/* Growth Ideas Grid */}
+      <div className="w-full flex flex-col gap-12">
+        <div className="w-3/5">{tile(tiles[0])}</div>
+
+        <div className="w-full flex flex-row gap-6">
+          {tile(tiles[1])}
+          {tile(tiles[2])}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BusinessGrowthPage;
